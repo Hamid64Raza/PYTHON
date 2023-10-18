80c92fa37ae28c291510cfc8c5655ae38b1c5d0f
Hamid Raza <112300409+Hamid64Raza@users.noreply.github.com>
2023-10-18 22:52:28 +0530

    Add files via upload

---
+++ matplotlib11.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 22:42:58 2023
+
+@author: Hamid Raza
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+Ypoint=np.array([11,22,33,30,20,15])
+plt.plot(Ypoint,color='#346534')
+plt.show()
---
+++ matplotlib9.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 22:38:07 2023
+
+@author: Hamid Raza
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+Ypoint=np.array([11,22,33,30,20,15])
+plt.plot(Ypoint,marker='o',ms=29,mec='m',mfc='k')
+plt.show()
