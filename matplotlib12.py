18f28b06dfdc3618afb46fb7853b9fdf4d0b0216
Hamid Raza <112300409+Hamid64Raza@users.noreply.github.com>
2023-10-18 22:53:22 +0530

    Add files via upload

---
+++ matplotlib12.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 22:43:58 2023
+
+@author: Hamid Raza
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+Ypoint=np.array([11,22,33,30,20,15])
+plt.plot(Ypoint, linewidth='20.2')
---
+++ matplotlib13.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 22:46:10 2023
+
+@author: Hamid Raza
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+Ypoint=np.array([11,22,33,30,20,15])
+Xpoint=np.array([10,9,7,5,6,8,10])
+plt.plot(Xpoint)
+plt.plot(Ypoint)
+plt.show()
