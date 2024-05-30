0284c4ee0118309100cab23236cb91ea182557fb
Hamid Raza <112300409+Hamid64Raza@users.noreply.github.com>
2024-05-30 23:14:02 +0530

    Update matplotlib1.py

--- matplotlib1.py
+++ matplotlib1.py
@@ -8,7 +8,7 @@ Created on Wed Oct 18 22:10:36 2023
 import matplotlib.pyplot as plt
 import numpy as np
 
-Xpoint=np.array([0,6])
-Ypoint=np.array([0,20])
+Xpoint=np.array([0,12])
+Ypoint=np.array([0,40])
 plt.plot(Xpoint,Ypoint)
 plt.show()
